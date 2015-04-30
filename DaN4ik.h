69bba4510e23c976be6d9a2cfe746b6605aa95f3
Danil <zaharenkodanil1@gmail.com>
2015-04-30 23:00:43 +0700

    quadeq

--- DaN4ik.h
+++ DaN4ik.h
@@ -18,3 +18,24 @@ sum+=a[i];
 }
 return sum;
 }
+
+template <typename T>
+T D4_quadeq(T a, T b, T, c)
+{
+    double D=b*b-4*a*c;
+    if (D>0)
+        {
+            double x1=(-b+sqrt(D))/(2*a);
+            double x2=(b+sqrt(D))/(2*a);
+            return x1, x2;
+        }
+        if (D==0)
+        {
+            double x=-b/(2*a);
+            return x;
+        }
+        if (D<0)
+        {
+            return 0;
+        }
+}
